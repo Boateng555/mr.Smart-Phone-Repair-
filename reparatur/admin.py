411257c530d70b5b3c1e757d1303a5fa3dc35d2d
Boateng555 <kwameb792@gmail.com>
2025-04-13 22:45:20 +0200

    Added confirmation email to repair request

--- reparatur/admin.py
+++ reparatur/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import RepairRequest
+
+@admin.register(RepairRequest)
+class RepairRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'device_type', 'damage_type', 'email', 'phone', 'payment_method', 'created_at')
+    search_fields = ('name', 'email', 'device_type', 'damage_type')
+    list_filter = ('device_type', 'damage_type', 'payment_method')
