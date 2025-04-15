411e2ef529f715e58bee5fe739c6b9fd126194eb
Boateng555 <kwameb792@gmail.com>
2025-04-15 16:08:49 +0200

    Switch to Supabase database

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,14 +74,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
+
+
+
+load_dotenv()  # Make sure this is called early
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mrsmart',
-        'USER': 'mrsmart',
-        'PASSWORD': '!23456789mr',
-        'HOST': 'mrsmart.postgres.database.azure.com',
-        'PORT': '5432',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
